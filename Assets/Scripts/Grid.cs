80d11aa0619a18234d2c0fb29606cd2a942b3627
TomDotScott <tom.scott45456@gmail.com>
2020-12-12 17:35:25 +0000

    Getting and Setting grid values
    On mouse click, the grid values can be changed

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -9,6 +9,7 @@ public class Grid
     private int m_height;
     private float m_cellSize;
     private int[,] m_gridArray;
+    private TextMesh[,] m_debugTextArray;
 
     public Grid(int width, int height, float cellSize)
     {
@@ -17,12 +18,13 @@ public class Grid
         m_cellSize = cellSize;
 
         m_gridArray = new int[width, height];
+        m_debugTextArray = new TextMesh[width, height];
 
         for (int x = 0; x < m_gridArray.GetLength(0); x++)
         {
             for (int y = 0; y < m_gridArray.GetLength(1); y++)
             {
-                UtilsClass.CreateWorldText(m_gridArray[x, y].ToString(), null, GetWorldPosition(x, y) + new Vector3(cellSize, cellSize) * 0.5f, 10, Color.white, TextAnchor.MiddleCenter);
+                m_debugTextArray[x, y] = UtilsClass.CreateWorldText(m_gridArray[x, y].ToString(), null, GetWorldPosition(x, y) + new Vector3(cellSize, cellSize) * 0.5f, 10, Color.white, TextAnchor.MiddleCenter);
                 Debug.Log(x + " " + y);
                 Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
                 Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100f);
@@ -30,11 +32,48 @@ public class Grid
         }
         Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
         Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
-
     }
 
     public Vector3 GetWorldPosition(int x, int y)
     {
         return new Vector3(x, y) * m_cellSize;
+    }
+
+    private Vector2Int GetXY(Vector3 worldPosition)
+    {
+        return new Vector2Int(Mathf.FloorToInt(worldPosition.x / m_cellSize), Mathf.FloorToInt(worldPosition.y / m_cellSize));
+    }
+
+    public void SetValue(int x, int y, int val)
+    {
+        // Ignore invalid values
+        if (x >= 0 && y >= 0 && x < m_width && y < m_height)
+        {
+            m_gridArray[x, y] = val;
+            m_debugTextArray[x, y].text = val.ToString();
+        }
+    }
+
+    public void SetValue(Vector3 worldPosition, int val)
+    {
+        Vector2Int xy = GetXY(worldPosition);
+        SetValue(xy.x, xy.y, val);
+    }
+
+    public int GetValue(int x, int y)
+    {
+        // Ignore invalid values
+        if (x >= 0 && y >= 0 && x < m_width && y < m_height)
+        {
+            return m_gridArray[x, y];
+        }
+
+        return -1;
+    }
+
+    public int GetValue(Vector3 worldPosition)
+    {
+        Vector2Int xy = GetXY(worldPosition);
+        return GetValue(xy.x, xy.y);
     }
 }
--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Testing : MonoBehaviour
 {
+    private Grid m_grid;
+
     // Start is called before the first frame update
     void Start()
     {
-        Grid grid = new Grid(4, 2, 3f);
+        m_grid = new Grid(4, 2, 3f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Set values on a mouse click
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Get the vector3 mouse position
+            Vector3 vec = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            // if it's a 0, set it to 1, if it's a 1, set it to 0
+            m_grid.SetValue(vec, m_grid.GetValue(vec) == 0 ? 1 : 0);
+        }
     }
 }
